576077dccfacafd607526a9229380c2ac691964d
Philip Romain <r_philip1@stu-digital-campus.fr>
2023-03-01 19:32:19 +0100

    ajout dossier ex01

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,8 @@
+/*Écrire une fonction qui affiche l’alphabet en majuscule, une string
+contenant l’alphabet en minuscule sera passée en paramètre de la
+fonction. Chaque lettre devra être stockée dans une cellule du
+tableau.*/
+
+let string ='alphabet';
+
+alert( str.split('abcdefghijklmnopqrstuvwxyz'));
